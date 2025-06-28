ffebe1195f7a31771a04c94b3234a53bbd92f120
martxgomez <martagomezmartinez95@gmail.com>
2025-06-28 22:53:06 +0200

    projects, about me and reviews final touches

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -42,6 +42,24 @@ export const projects = [
 
 export const reviews = [
   {
+    name: "Tomas Martin",
+    image: {
+      src: "./assets/Tomas.jpeg",
+      alt: "Imagen de Tomas",
+    },
+    reviewText:
+      "Marta aporta un valor enorme a cualquier equipo en el que participa. Durante el bootcamp de desarrollo web en Ironhack coincidimos en varios proyectos, y siempre me impresionó su capacidad para enfrentarse a cualquier reto con determinación y una actitud constructiva. Tiene una gran facilidad para aprender rápido, colaborar de forma efectiva y mantener la motivación del grupo incluso en los momentos más intensos. Además, su compañerismo y disposición para ayudar hacen que trabajar con ella sea realmente enriquecedor. Una desarrolladora con mucho talento, preparada para afrontar con éxito cualquier desafío que se proponga.",
+  },
+  {
+    name: "Èric Masip",
+    image: {
+      src: "./assets/Eric.jpeg",
+      alt: "Imagen de Èric",
+    },
+    reviewText:
+      "Marta es una persona que destaca por su visión de proyecto, su capacidad organizativa y su proactividad. Es capaz de encontrar soluciones creativas y eficientes a situaciones complejas sin perder la sonrisa y la buena actitud que la caracterizan. Sin duda es una persona que aporta mucha frescura y dinamismo a un equipo, pues su inagotable energía se contagia fácilmente a todo aquél con quien trabaja. Yo no tuve ocasión de formar grupo con ella durante mi formación, pero ver sus progresos, su dedicación y el producto de su esfuerzo reflejado en los excelentes proyectos que presentó junto a sus compañeros (siempre de forma amena y divertida a la par que profesional) fue y sigue siendo una inspiración, como seguro lo será para sus futuros compañeros y compañeras de trabajo entre los que ojalá me encuentre algún día. La recomiendo encarecidamente para cualquier empresa que busque talento, implicación y ganas de seguir mejorando y creciendo para ofrecer lo mejor a sus clientes.",
+  },
+  {
     name: "Roxana Ferramola",
     image: {
       src: "./assets/Roxana.jpeg",
